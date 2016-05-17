fd7cafff690b350bd273d6ce8d5b95b4edc7d9aa
calcorum <calcorum@users.noreply.github.com>
2016-05-17 09:17:21 -0400

    Initial upload - excludes static images

---
+++ definitions.py
@@ -0,0 +1,30 @@
+class switchObj:
+    address = ''
+    name = ''
+    slave = None
+
+    def __init__(self):
+        self.name = ''
+
+    def __init__(self, name, address, slave=None):
+        self.name = name
+        self.address = address
+        self.slave = slave
+
+# First Floor Switches
+kitchen = switchObj('Kitchen','38 1C 89 1',switchObj('Kitchen','38 18 E0 1'))
+diningRoom = switchObj('DiningRoom','38 27 D9 1',switchObj('DiningRoom','38 1F 74 1'))
+livingRoom = switchObj('LivingRoom','38 F 43 1',switchObj('LivingRoom','38 18 F2 1'))
+
+# Outside Switches
+frontPorch = switchObj('FrontPorch','38 A0 AA 1')
+garageOutdoor = switchObj('GarageOutdoor','38 9E 37 1')
+lightPost = switchObj('LightPost','38 F 84 1')
+
+ISYADDRESS = 'http://10.0.0.240/rest/'
+ISYADMIN = 'admin'
+ISYPASSWORD = 'Share91!'
+FIRST_FLOOR = [kitchen, diningRoom, livingRoom]
+OUTSIDE = [frontPorch, garageOutdoor, lightPost]
+
+
---
+++ isy-dashboard.py
@@ -0,0 +1,116 @@
+from flask import Flask, abort, request, render_template, redirect
+from xml.etree import ElementTree
+import requests, time, definitions
+
+app = Flask(__name__)
+
+isy_rest_address = definitions.ISYADDRESS
+isy_admin = definitions.ISYADMIN
+isy_pwd = definitions.ISYPASSWORD
+sleep = .25
+
+@app.before_request
+def limit_remote_addr():
+	if '10.0.0.' not in request.remote_addr:
+                if '10.10.0.' not in request.remote_addr:
+                        abort(404)
+
+@app.route('/')
+@app.route('/index/')
+def index():
+	return 'Home page'
+
+@app.route('/isy/')
+def root():
+	return render_template('isy_root.html')
+
+@app.route('/isy/<group>/')
+def specific_group(group):
+        nodes = get_nodes(group)
+	if not nodes:
+		abort(404)
+
+        nodeStatus = {}
+	for switch in nodes:
+                nodeStatus[switch.name] = get_status(switch)
+        
+	return render_template('isy_' + group + '.html',status=nodeStatus)
+
+@app.route('/isy/<group>/all_off/')
+def all_off(group):
+        nodes = get_nodes(group)
+        if not nodes:
+                # Would like to display an error message to the user here
+                return redirect('/isy/' + group + '/', code = 302)
+
+        for switch in nodes:
+                light_off(switch)
+
+        time.sleep(sleep)
+        return redirect('/isy/' + group + '/', code = 302)
+
+@app.route('/isy/<group>/all_on/')
+def all_on(group):
+        nodes = get_nodes(group)
+        if not nodes:
+                # Would like to display an error message to the user here
+                return redirect('/isy/' + group + '/', code = 302)
+
+        for switch in nodes:
+                light_on(switch)
+
+        time.sleep(sleep)
+        return redirect('/isy/' + group + '/', code = 302)
+
+@app.route('/isy/<group>/<name>/toggle/')
+def toggle(group, name):
+        nodes = get_nodes(group)
+        if not nodes:
+                # Would like to display an error message to the user here
+                return redirect('/isy/' + group + '/', code = 302)
+
+        for switch in nodes:
+                if switch.name == name:
+                        if get_status(switch) == 'On':
+                                light_off(switch)
+                        else:
+                                light_on(switch)
+
+        time.sleep(sleep)
+        return redirect('/isy/' + group + '/', code = 302)
+
+# The if/elif statements will need to be updated to use the group names called by your html toggle function
+def get_nodes(group):
+        """Return a list of switchObj objects
+
+        Argument:
+        group -- the name as listed in definitions.py of the group of switches"""
+        if group == 'first':
+                return definitions.FIRST_FLOOR
+        elif group == 'outside':
+                return definitions.OUTSIDE
+        else:
+                return None
+
+def light_off(switch):
+        """Sends the off command to the address of the passed in switchObj"""
+        requests.get(isy_rest_address+'/nodes/'+switch.address+'/cmd/DOF/',auth=(isy_admin,isy_pwd))
+        if switch.slave:
+                light_off(switch.slave)
+                
+def light_on(switch):
+        """Sends the on command to the address of the passed in switchObj"""
+        requests.get(isy_rest_address+'/nodes/'+switch.address+'/cmd/DON/',auth=(isy_admin,isy_pwd))
+        if switch.slave:
+                light_on(switch.slave)
+
+def get_status(switch):
+        """Returns the formatted status from ISY of the switchObj passed in"""
+	nodeXml = requests.get(isy_rest_address+'nodes/'+switch.address,auth=(isy_admin,isy_pwd))	
+	nodeData = ElementTree.fromstring(nodeXml.content)
+	for prop in nodeData.findall('properties'):
+		return prop[2].attrib['formatted']
+        
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0')
